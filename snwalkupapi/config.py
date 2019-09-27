a2d4d902726b8f1c5c6e821e54278793954f52dc
Tyler <tyjohn134@gmail.com>
2019-09-27 09:39:17 -0400

    changed config file

--- snwalkupapi/config.py
+++ snwalkupapi/config.py
@@ -6,15 +6,15 @@ servicenow = dict(
 )
 
 location = dict(
-    loc_id = 'c057c2880f5fe1002a884b9ce1050e64'
+    loc_id = ''
 )
 
 u_business_service = dict(
-    bus_id = 'a3b412b00f3ee100d7f84b9ce1050e31'
+    bus_id = ''
 )
 
 assignment_group = dict(
-    asssign_id = 'ee30ba480f872500d7f84b9ce1050ebc'
+    asssign_id = ''
 )
 
 tech_emails = {
